52d233d7d902eb4fbb911162af752e28b64faf87
Karan <www.karanofficial111@fmail.com>
2026-08-02 01:07:11 +0530

    add reverse code

--- count.java
+++ count.java
@@ -18,25 +18,56 @@ public static void main(String[] args){
   // }
   // System.out.println("number of digit in " +original+"="+num);
 
-int sum=0;
+
+
+
+// int sum=0;
+
+// while(n>0){
+//   int digit=n%10;
+//    sum+=digit;
+
+//    n/=10;
+
+
+// }
+// System.out.println(sum);
+
+
+
+
+
+// }
+
+
+
+
+
+
+int ans=0;
 
 while(n>0){
   int digit=n%10;
-   sum+=digit;
-
-   n/=10;
-
-
-}
-System.out.println(sum);
-
-
-
-
+  ans =ans*10+digit;
+n/=10;
 
 }
 
+System.out.println(ans);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+}
 }
