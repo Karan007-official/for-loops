40025a5c428a11151a55a4a84585d745e324cbda
Karan <www.karanofficial111@fmail.com>
2026-08-02 02:11:39 +0530

    second pettern

--- pattern.java
+++ pattern.java
@@ -7,11 +7,26 @@ public class pattern {
 
         int n = sc.nextInt();
         int  x = sc.nextInt();
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=x;j++){
+//         System.out.print("*");
+//     }
+//     System.out.println();
+// }
+
+
+
 for(int i=1;i<=n;i++){
     for(int j=1;j<=x;j++){
-        System.out.print("*");
+        if(i==1 || i==n || j==1 || j==x)
+            System.out.print("*");
+        else
+            System.out.print(" ");
     }
-    System.out.println();
+System.out.println();
 }
+
+
+
     }
 }
