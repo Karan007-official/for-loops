84eaba55efc60c8c30cc769d1998c6a050814da3
Karan <www.karanofficial111@fmail.com>
2026-08-02 02:15:21 +0530

    third pattern

--- pattern.java
+++ pattern.java
@@ -6,7 +6,7 @@ public class pattern {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int  x = sc.nextInt();
+        // int  x = sc.nextInt();
 // for(int i=1;i<=n;i++){
 //     for(int j=1;j<=x;j++){
 //         System.out.print("*");
@@ -16,15 +16,25 @@ public class pattern {
 
 
 
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=x;j++){
+//         if(i==1 || i==n || j==1 || j==x)
+//             System.out.print("*");
+//         else
+//             System.out.print(" ");
+//     }
+// System.out.println();
+// }
+
+
 for(int i=1;i<=n;i++){
-    for(int j=1;j<=x;j++){
-        if(i==1 || i==n || j==1 || j==x)
-            System.out.print("*");
-        else
-            System.out.print(" ");
+    for(int j=1;j<=i;j++ ){
+        System.out.print("*");
     }
-System.out.println();
+    System.out.println();
 }
+
+
 
 
 
